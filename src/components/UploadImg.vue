772726213f5e7f8d9560588058e23aa71ad32899
Mizu <psdk55052004@gmail.com>
2022-07-14 17:55:10 +0800

    新增 取得檔案資訊方法

--- src/components/UploadImg.vue
+++ src/components/UploadImg.vue
@@ -1,5 +1,8 @@
 <template>
-檔案上傳
+  <label for="file"></label>
+  <input type="file" id="file " name="file" @change="getFileInfo">
+
+  <p ref="uploadResult"></p>
 </template>
 
 <script>
@@ -10,7 +13,8 @@ export default {
 
   data () {
     return {
-      type: []
+      file: {},
+      type: ['png', 'jpg', 'svg', 'jpeg', 'bmp', 'gif']
     }
   },
 
@@ -18,6 +22,31 @@ export default {
   },
 
   methods: {
+    getFileInfo (e) {
+      const file = e.target.files[0]
+      console.log(file)
+      const type = file.name.split('.').pop() //* 從檔名最後面來取得檔案類型
+      const size = file.size
+      //* 驗證圖片類型、大小，合格才繼續執行
+      // this.checkFile(file)
+      const typeValidate = this.checkType(type)
+      const sizeValidate = this.checkSize(size)
+      if (!typeValidate && !sizeValidate) return //* 其中一個驗證失敗就中斷
+      const name = file.name.split('.')[0]
+      const lastModifiedDate = file.lastModifiedDate.toLocaleString()
+      const uploadDate = new Date().toLocaleString()
+      this.file = { uploadDate, name, size, type, lastModifiedDate }
+      console.log('gg')
+    },
+    // checkFile (file) {
+    // this.checkType(file)
+    // this.checkSize(file)
+    // },
+    checkType (type) {
+    },
+    checkSize (size) {
+    }
+
   },
 
   mounted () {
@@ -26,4 +55,5 @@ export default {
 }
 </script>
 
-<style lang='scss' scope></style>
+<style lang='scss' scope>
+</style>
