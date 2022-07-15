80de7a57eeb05bf7a4aeb69ae7670d84e731d2d2
Mizu <psdk55052004@gmail.com>
2022-07-15 11:57:46 +0800

    新增 進度條元件 - 架構

---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,42 @@
+<template>
+  <label ref="progressLabel" for="progress"></label>
+  <progress ref="progress" value="0" max="100"> </progress>
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+
+  data () {
+    return {
+    }
+  },
+
+  watch: {
+  },
+
+  methods: {
+    getProgressBar () {
+      const reader = new FileReader()
+      reader.addEventListener('progress', (event) => {
+        if (event.loaded && event.total) {
+          // 計算完成百分比
+          const percent = (event.loaded / event.total) * 100
+          // 將值設定為進度元件
+          // progress.value = percent
+          this.$refs.progress.value = percent
+          this.$refs.progressLabel.innerHTML = Math.round(percent) + '%'
+        }
+      })
+    }
+  },
+
+  mounted () {
+  }
+
+}
+</script>
+
+<style lang='scss' scope></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,15 @@
 <template>
-  <UploadImg></UploadImg>
+  <section class="my-3">
+    <h2>上傳 + 驗證</h2>
+    <UploadImg :size="size" :type="type" :width="width" :height="height"></UploadImg>
+  </section>
+
+  <hr />
+
+  <section class="my-3">
+    <h2>測試：props 參數,<code>size:165800</code>, <code>w:150</code>, <code>h:150</code></h2>
+    <UploadImg :size="165800" :type="type" :width="150" :height="150"></UploadImg>
+  </section>
 </template>
 
 <script>
@@ -11,7 +21,12 @@ export default {
 
   data () {
     return {
-      isLoading: false
+      isLoading: false,
+      size: 31457280,
+      type: ['png', 'jpg', 'svg', 'jpeg', 'bmp', 'gif'],
+      width: 400,
+      height: 400
+
     }
   },
 
