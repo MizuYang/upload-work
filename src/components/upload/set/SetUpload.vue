915bd145cef8c16443de1404462d285c4e920036
Mizu <psdk55052004@gmail.com>
2022-07-28 10:24:17 +0800

    新增 上傳類型區塊

--- src/components/upload/set/SetUpload.vue
+++ src/components/upload/set/SetUpload.vue
@@ -3,29 +3,97 @@
     <header class="text-center mb-3">
       <h2>上傳設定</h2>
     </header>
-    <section class="">
-      上傳模式：
+    <main>
+      <!-- 上傳模式：
       <select name="" id="" v-model="options.uploadMode">
         <option value="一般檔案">一般檔案</option>
         <option value="圖片">圖片</option>
       </select>
-      <br /> <br />
-      <!-- 限制檔案大小 -->
+      <br /> <br /> -->
 
-  <!-- 上傳格式設定 -->
+    <!-- 上傳格式設定 -->
       <!-- Word 檔案類型 -->
+      <section class="border-bottom mb-2 pb-2">
+        <h5>Word 類型</h5>
+        <div class="d-flex flex-wrap flex-wrap">
+          <div v-for="format in format.word" :key="format" class="mx-1">
+            <input type="checkbox" :id="format" class="me-1">
+            <label :for="format">{{ format }}</label>
+          </div>
+        </div>
+      </section>
 
       <!-- Excel檔案類型 -->
-      <!-- ppT -->
-      <!-- PDF 檔案類型 -->
-      <!-- 影像檔案類型 -->
-      <!-- 視訊檔案類型 -->
-      <!-- 音訊檔案類型 -->
+      <section class="border-bottom mb-2 pb-2">
+        <h5>Excel 類型</h5>
+        <div class="d-flex flex-wrap">
+          <div v-for="format in format.excel" :key="format" class="mx-1">
+            <input type="checkbox" :id="format" class="me-1">
+            <label :for="format">{{ format }}</label>
+          </div>
+        </div>
+      </section>
 
+      <!-- PPT -->
+      <section class="border-bottom mb-2 pb-2">
+        <h5>PPT 類型</h5>
+        <div class="d-flex flex-wrap">
+          <div v-for="format in format.ppt" :key="format" class="mx-1">
+            <input type="checkbox" :id="format" class="me-1">
+            <label :for="format">{{ format }}</label>
+          </div>
+        </div>
+      </section>
+
+      <!-- PDF 檔案類型 -->
+      <section class="border-bottom mb-2 pb-2">
+        <h5>PDF 類型</h5>
+        <div class="d-flex flex-wrap">
+          <div v-for="format in format.pdf" :key="format" class="mx-1">
+            <input type="checkbox" :id="format" class="me-1">
+            <label :for="format">{{ format }}</label>
+          </div>
+        </div>
+      </section>
+
+      <!-- 影像檔案類型 -->
+      <section class="border-bottom mb-2 pb-2">
+        <h5>圖片類型</h5>
+        <div class="d-flex flex-wrap">
+          <div v-for="format in format.img" :key="format" class="mx-1">
+            <input type="checkbox" :id="format" class="me-1">
+            <label :for="format">{{ format }}</label>
+          </div>
+        </div>
+      </section>
+
+      <!-- 視訊檔案類型 -->
+      <section class="border-bottom mb-2 pb-2">
+        <h5>影片類型</h5>
+        <div class="d-flex flex-wrap">
+          <div v-for="format in format.video" :key="format" class="mx-1">
+            <input type="checkbox" :id="format" class="me-1">
+            <label :for="format">{{ format }}</label>
+          </div>
+        </div>
+      </section>
+
+      <!-- 音訊檔案類型 -->
+      <section class="border-bottom mb-2 pb-2">
+        <h5>音訊類型</h5>
+        <div class="d-flex flex-wrap">
+          <div v-for="format in format.music" :key="format" class="mx-1">
+            <input type="checkbox" :id="format" class="me-1">
+            <label :for="format">{{ format }}</label>
+          </div>
+        </div>
+      </section>
+
+    <!-- 限制檔案大小 -->
       <label for="validateSize">檔案限制大小：</label>
       <input id="validateSize" type="number" placeholder="檔案大小限制" v-model="options.validateSize" oninput="value=value.replace(/[^\d]/g,'')">
       <br /> <br />
-      <!-- 限制圖片寬高 -->
+    <!-- 限制圖片寬高 -->
       <div v-if="options.uploadMode === '圖片'">
         <input type="checkbox" id="resolution" v-model="options.validateResolution">
         <label for="resolution">我要限制圖片寬高</label>
@@ -37,10 +105,12 @@
           <input type="number" id="height" class="inputSize" v-model="options.validateH" oninput="value=value.replace(/[^\d]/g,'')">
         </div>
       </div>
-    </section>
+    </main>
 
     <!-- 開始上傳按鈕 -->
     <footer class="my-3 text-center">
+      <h3>已設定的項目</h3>
+      {{ options }}
       <button type="button" class="btn btn-primary" @click="startUpload">開始上傳</button>
     </footer>
 </template>
@@ -67,8 +137,6 @@ export default {
         uploadMode: '圖片',
         validateSize: 153600,
         validateResolution: false,
-        validateW: 0,
-        validateH: 0,
         setFinish: false
       },
       format: {
