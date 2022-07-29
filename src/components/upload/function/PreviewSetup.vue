7926f35b05b1328037b9f27e1ffb2a785b72e385
Mizu <psdk55052004@gmail.com>
2022-07-29 16:08:53 +0800

    修復 設定預覽資料尚未進來跳錯問題

--- src/components/upload/function/PreviewSetup.vue
+++ src/components/upload/function/PreviewSetup.vue
@@ -1,27 +1,27 @@
 <template>
-  <h3>已設定的項目</h3>
-      <ul class="px-0">
-        <li v-if="setup.size.hasValidateSize==='true' && setup.size.setSize">
-          <span class="fs-5">檔案限制大小：</span>
-          <span class="border-bottom">{{ setup.size.setSize }} {{ setup.size.unit }}</span>
-        </li>
-        <li v-if="setup.hasValidateResolution">
-          <span class="fs-5">圖片解析度：</span>
-          <span v-if="setup.validateW || setup.validateH" class="border-bottom">
-            {{ setup.validateW }} * {{ setup.validateH }}
-          </span>
-        </li>
-        <li v-if="setup.mode!=='請選擇檔案格式'">
-          <span class="fs-5">上傳模式：</span>
-          <span class="border-bottom">{{ setup.modeList[setup.mode] }}</span>
-        </li>
-        <li v-if="setup.validateType.length > 0">
-          <span class="fs-5">檔案格式：</span> <br />
-          <code v-for="type in setup.validateType" :key="type" class="symbol border-bottom">
-            {{ type }}
-          </code>
-        </li>
-      </ul>
+    <!-- <h3>已設定的項目</h3> -->
+    <ul class="px-0">
+      <li v-if="setup.size?.hasValidateSize==='true' && setup.size?.setSize">
+        <span class="fs-5">檔案限制大小：</span>
+        <span class="border-bottom">{{ setup.size.setSize }} {{ setup.size.unit }}</span>
+      </li>
+      <li v-if="setup.hasValidateResolution">
+        <span class="fs-5">圖片解析度：</span>
+        <span v-if="setup.validateW || setup.validateH" class="border-bottom">
+          {{ setup.validateW }} * {{ setup.validateH }}
+        </span>
+      </li>
+      <li v-if="setup.mode !== '請選擇檔案格式'">
+        <span class="fs-5">上傳模式：</span>
+        <span class="border-bottom">{{ uploadMode }}</span>
+      </li>
+      <li v-if="setup.validateType?.length > 0">
+        <span class="fs-5">檔案格式：</span> <br />
+        <code v-for="type in setup.validateType" :key="type" class="symbol border-bottom">
+          {{ type }}
+        </code>
+      </li>
+    </ul>
 </template>
 
 <script>
@@ -31,6 +31,23 @@ export default {
     setup: {
       type: Object
     }
+  },
+
+  computed: {
+    uploadMode () {
+      let resolut = ''
+      //* 若有資料再讀取才不會出錯
+      if (this.setup.modeList && this.setup.mode) {
+        resolut = this.setup.modeList[this.setup.mode]
+      }
+      return resolut
+    }
+  },
+
+  mounted () {
+    // setInterval(() => {
+    //   console.log(this.setup)
+    // }, 2000)
   }
 }
 </script>
--- src/components/upload/set/SetUploadItem.vue
+++ src/components/upload/set/SetUploadItem.vue
@@ -1,4 +1,64 @@
-<template>內容</template>
+<template>
+      <h2 class="text-center mb-3" v-if="!setup.setFinish">上傳設定</h2>
+      上傳格式：
+      <select v-model="setup.mode" @change="changeMode">
+        <option selected="請選擇檔案格式" disabled>請選擇檔案格式</option>
+        <option :value="key" v-for="(mode, key) in setup.modeList" :key="mode">{{ mode }}</option>
+      </select>
+
+      <i class="text-danger ms-2 d-none" ref="mode">*此欄位必填</i>
+
+      <br /> <br />
+
+      <!-- 上傳格式設定 -->
+      <section class="border-bottom mb-2 pb-2" v-for="(type, key) in setup.modeList" :key="key" v-show="setup.mode === key">
+        <h5>
+          <button type="button" class="btn btn-dark btn-sm" @click.self="allCheck(key)">{{ type }} 類型</button>
+          <i class="text-danger ms-2 d-none" :ref="key">*此欄位必填</i>
+        </h5>
+        <div class="d-flex flex-wrap flex-wrap">
+          <div v-for="item in typeList" :key="item" class="mx-1">
+            <input type="checkbox" :value="item" :id="item" class="me-1" v-model="setup.validateType">
+            <label :for="item">{{ item }}</label>
+          </div>
+        </div>
+      </section>
+
+      <!-- 限制檔案大小 -->
+      <input type="radio" name="size" id="freeSize" value="false" v-model="setup.size.hasValidateSize">
+      <label for="freeSize">大小無限制</label>
+
+      <input type="radio" name="size" id="sizeRule" class="ms-3" value="true" v-model="setup.size.hasValidateSize">
+      <label for="sizeRule">限制檔案大小</label>
+
+      <template v-if="setup.size.hasValidateSize==='true'">
+        <select class="mx-2" v-model="setup.size.unit">
+          <option :value="unit" v-for="unit in setup.size.unitList" :key="unit">{{ unit }}</option>
+        </select>
+
+        <label for="validateSize" class="ms-3">檔案大小：</label>
+        <input id="validateSize" type="number" v-model="setup.size.setSize" oninput="value=value.replace(/[^\d]/g,'')"
+          :class="inputSize">
+          {{ setup.size.unit }}
+        <i class="text-danger ms-2 d-none" ref="size">*此欄位必填</i>
+      </template>
+      <br /> <br />
+
+      <!-- 限制圖片寬高 -->
+      <div v-if="setup.mode === 'img'">
+        <input type="checkbox" id="resolution" v-model="setup.hasValidateResolution">
+        <label for="resolution">我要限制圖片寬高</label>
+        <div v-if="setup.hasValidateResolution" class="my-3">
+          <label for="width">寬：</label>
+          <input type="number" id="width" class="inputSize" v-model="setup.validateW" oninput="value=value.replace(/[^\d]/g,'')">
+          <i class="text-danger ms-2 d-none" ref="w">*此欄位必填</i>
+          <br /> <br />
+          <label for="height">高：</label>
+          <input type="number" id="height" class="inputSize" v-model="setup.validateH" oninput="value=value.replace(/[^\d]/g,'')">
+          <i class="text-danger ms-2 d-none" ref="h">*此欄位必填</i>
+        </div>
+      </div>
+</template>
 
 <script>
 
@@ -6,12 +66,74 @@ export default {
   components: {
   },
 
-  data () {
-    return {
+  emits: ['getSetup'],
+
+  watch: {
+    setup: {
+      handler () {
+        this.$emit('getSetup', this.setup)
+      },
+      deep: true,
+      immediate: true
     }
   },
 
-  watch: {
+  computed: {
+    inputSize () {
+      const style = this.setup.size.unitStyle
+      const unit = this.setup.size.unit
+      return style[unit]
+    },
+    typeList () {
+      const mode = this.setup.mode
+      return this.type[mode]
+    }
+  },
+
+  data () {
+    return {
+      allCheckCount: {},
+      setup: {
+        modeList: {
+          word: 'Word',
+          excel: 'Excel',
+          ppt: 'PPT',
+          pdf: 'PDF',
+          img: '圖片',
+          video: '影片',
+          music: '音訊'
+        },
+        mode: '請選擇檔案格式', //* 用戶選擇上傳模式：Word、PTT..等
+        validateType: [], //* 能上傳的格式
+        size: {
+          unitList: ['GB', 'MB', 'KB', 'B'],
+          unitStyle: {
+            GB: 'w-s',
+            MB: 'w-m',
+            KB: 'w-l',
+            B: 'w-xl'
+          },
+          unit: 'KB',
+          setSize: 150,
+          validateSize: 0,
+          hasValidateSize: false
+        },
+        hasValidateResolution: false,
+        validateW: 0,
+        validateH: 0,
+        setFinish: false
+      },
+      type: {
+        word: ['doc', 'dot', 'wbk', 'docx', 'docm', 'dotx', 'dotm', 'docb'],
+        excel: ['xls', 'xlt', 'xlm', 'xlsx', 'xlsm', 'xltx', 'xltm'],
+        ppt: ['ppt', 'pot', 'pps', 'pptx', 'pptm', 'potx', 'potm', 'ppam', 'ppsx', 'ppsm', 'sldx', 'sldm'],
+        pdf: ['pdf', 'accdb'],
+        img: ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'tiff', 'psd', 'thm', 'yuv', 'ai', 'drw', 'eps', 'ps', 'svg', '3dm', 'max', 'heic', 'heif'],
+        video: ['avi', 'mp4', 'mov', 'wmv', 'asf', '3g2', '3gp', 'asx', 'flv', 'mpg', 'rm', 'swf', 'vob'],
+        music: ['mp3', 'aif', 'iff', 'm3u', 'm4a', 'mid', 'mpa', 'ra', 'wav', 'wma']
+
+      }
+    }
   },
 
   methods: {
